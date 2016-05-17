b312bb7979cbdd929440e9705e8560d1057a29f0
Amith <amitn.rose@gmail.com>
2016-05-17 17:27:37 +0530

    commit the app class

---
+++ src/main/java/com/dman/StartUp.java
@@ -0,0 +1,18 @@
+package com.dman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author amithpan
+ *
+ */
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(StartUp.class, args);
+	}
+}
